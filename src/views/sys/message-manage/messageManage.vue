4862f2653bc90c012f46a24f34ea9ee4d5764322
yangm0910 <yangm_1111@163.com>
2019-04-18 17:50:34 +0800

    add message page

--- src/views/sys/message-manage/messageManage.vue
+++ src/views/sys/message-manage/messageManage.vue
@@ -1,25 +1,29 @@
 <template>
   <div class="message">
     <div class="left-contact">
-    <Table height="800" :columns="columns1" :data="data2"></Table>
+      <Table height="800" :columns="columns1" :data="data2"></Table>
 
       <!-- <ul>
         <li>111</li>
         <li>222</li>
-      </ul> -->
+      </ul>-->
     </div>
     <div class="right-chatwindow">
       <div class="message-record">
-        <div><h1>people</h1></div>
+        <div class="contactor">
+          <h1>people</h1>
+        </div>
         <ul>
-          <li>hi</li>
-          <li>hi</li>
-          <li>hi</li>
+          <li class="single-msg">hi</li>
+
+          <li class="single-msg">hi</li>
+
+          <li class="single-msg">hi</li>
         </ul>
       </div>
       <div class="message-input">
-        <input v-model="inputValue" id="text" type="text">
-        <button @click="sendMsg">Send</button>
+        <Input v-model="value" placeholder="Enter something..." style="width: 80%"/>
+        <Button type="primary" @click="sendMsg"  style="width: 20%">Primary</Button>
       </div>
     </div>
     <!-- <Table height="200" :columns="columns1" :data="data2"></Table>
@@ -38,7 +42,7 @@ import Cookies from "js-cookie";
 export default {
   data() {
     return {
-      inputValue: "hello",
+      value: "hello",
       websocket: null,
       messageRecords: [],
       columns1: [
@@ -50,26 +54,26 @@ export default {
       data2: [{ name: "test" }, { name: "test2" }]
     };
   },
-  
+
   methods: {
     init() {
-    //判断当前浏览器是否支持WebSocket
-    let userInfo = JSON.parse(Cookies.get("userInfo"));
-    let username = userInfo.username;
-    if ("WebSocket" in window) {
-      this.websocket = new WebSocket(
-        "ws://127.0.0.1:8888/xboot/websocket/" + username
-      );
+      //判断当前浏览器是否支持WebSocket
+      let userInfo = JSON.parse(Cookies.get("userInfo"));
+      let username = userInfo.username;
+      if ("WebSocket" in window) {
+        this.websocket = new WebSocket(
+          "ws://127.0.0.1:8888/xboot/websocket/" + username
+        );
 
-      this.websocket.onopen = function(e) {
-        console.log("link success");
-      };
-      this.websocket.onmessage = this.onMessage;
-      this.websocket.onclose = this.onClose;
-    } else {
-      alert("Not support websocket");
-    }
-  },
+        this.websocket.onopen = function(e) {
+          console.log("link success");
+        };
+        this.websocket.onmessage = this.onMessage;
+        this.websocket.onclose = this.onClose;
+      } else {
+        alert("Not support websocket");
+      }
+    },
     onMessage: function(e) {
       //收到消息
       console.log("reveive: " + e.data);
@@ -79,7 +83,7 @@ export default {
       console.log("colse");
     },
     sendMsg: function(msg) {
-      this.websocket.send(this.inputValue);
+      this.websocket.send(this.value);
     }
   }
 };
